78a9a2c07c419db24230f559d8ce17c93ddf7109
zzy <1203122901@qq.com>
2024-03-19 21:40:31 +0800

    upd:frontend before add backend

--- src/components/graph3.vue
+++ src/components/graph3.vue
@@ -7,54 +7,125 @@
 </template>
 
 <script>
-import Plotly from 'plotly.js-dist';
+import * as echarts from 'echarts';
+
 export default {
   mounted() {
-    this.loadData();
+    this.renderChart();
+    window.addEventListener('resize', this.handleResize);
   },
   methods: {
-    loadData() {
-      fetch('https://raw.githubusercontent.com/plotly/datasets/master/api_docs/mt_bruno_elevation.csv')
-        .then(response => response.text())
-        .then(data => {
-          const rows = data.split('\n').map(row => row.split(','));
-          
-          function unpack(rows, key) {
-            return rows.map(row => row[key]);
-          }
-          
-          let z_data = [];
-          for (let i = 0; i < 24; i++) {
-            z_data.push(unpack(rows, i));
-          }
-          
-          const trace = {
-            z: z_data,
-            type: 'surface'
-          };
-          
-          const layout = {
-            autosize: false,
-            width: 400,
-            height: 400,
-            margin: {
-              l: 10,
-              r: 10,
-              b: 10,
-              t: 10
+    generateData() {
+      let data = [];
+      let rowData = [
+    [null, null, null, null, null, 51, 52, 50, 51, 52, null, null, null, null, null],
+    [null, null, null, 46, 47, 48, 49, 45, 46, 47, 48, 49, null, null, null],
+    [null, null, 40, 41, 42, 43, 44, 39, 40, 41, 42, 43, 44, null, null],
+    [null, 26, 27, 28, 29, 30, 31, 25, 26, 27, 28, 29, 30, 31, null],
+    [null, 33, 34, 35, 36, 37, 38, 32, 33, 34, 35, 36, 37, 38, null],
+    [10, 11, 12, 13, 14, 15, 16, 9, 10, 11, 12, 13, 14, 15, 16],
+    [18, 19, 20, 21, 22, 23, 24, 7, 18, 19, 20, 21, 22, 23, 24],
+    [2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8],
+    [10, 11, 12, 13, 14, 15, 16, 9, 10, 11, 12, 13, 14, 15, 16],
+    [18, 19, 20, 21, 22, 23, 24, 7, 18, 19, 20, 21, 22, 23, 24],
+    [null, 26, 27, 28, 29, 30, 31, 25, 26, 27, 28, 29, 30, 31, null],
+    [null, 33, 34, 35, 36, 37, 38, 32, 33, 34, 35, 36, 37, 38, null],
+    [null, null, 40, 41, 42, 43, 44, 39, 40, 41, 42, 43, 44, null, null],
+    [null, null, null, 46, 47, 48, 49, 45, 46, 47, 48, 49, null, null, null],
+    [null, null, null, null, null, 51, 52, 50, 51, 52, null, null, null, null, null]
+  ];
+
+      for (let i = 0; i < rowData.length; i++) {
+        for (let j = 0; j < rowData[i].length; j++) {
+          data.push([j, i, rowData[i][j]]);
+        }
+      }
+
+      return data;
+    },
+    renderChart() {
+      let dom = document.getElementById('graph3');
+      let myChart = echarts.init(dom, null, {
+        renderer: 'canvas',
+        useDirtyRect: false
+      });
+
+
+        let option = {
+            xAxis: {
+            type: 'category',
+            data: Array.from({ length: 15 }, (_, i) => i + 1),
+            axisLabel: {
+                textStyle: {
+                color: 'white' // 将 x 轴标签文字颜色设置为白色
+                }
+            },
+            axisLine: {
+                lineStyle: {
+                color: 'white' // 将 x 轴线的颜色设置为白色
+                }
             }
-          };
-          
-          Plotly.newPlot('graph3', [trace], layout);
-        })
-        .catch(error => {
-          console.error('Error loading data:', error);
-        });
+            },
+            yAxis: {
+            type: 'category',
+            data: Array.from({ length: 15 }, (_, i) => i + 1),
+            axisLabel: {
+                textStyle: {
+                color: 'white' // 将 y 轴标签文字颜色设置为白色
+                }
+            },
+            axisLine: {
+                lineStyle: {
+                color: 'white' // 将 y 轴线的颜色设置为白色
+                }
+            }
+            },
+            textStyle: {
+            color: 'white' // 将其他所有文字元素的颜色设置为白色
+            },
+            visualMap: {
+            min: 0,
+            max: 52,
+            calculable: true,
+            realtime: false,
+            inRange: {
+                color: [
+                    '#ffffff', '#0000ff', '#00ffff', '#00ff00', '#ffff00', '#ff0000'
+                ]
+                }
+            },
+            series: [
+            {
+                name: 'Heatmap',
+                data: this.generateData(),
+                type: 'heatmap',
+                label: {
+                show: true,
+                position: 'inside',
+                formatter: function (params) {
+                    return params.value[2];
+                },
+                fontSize: 10,
+                color: 'black'
+                }
+            }
+            ]
+        };
+        // ECharts配置项
+
+  if (option && typeof option === 'object') {
+        myChart.setOption(option);
+      }
+    },
+    handleResize() {
+      this.myChart.resize();
     }
+  },
+  beforeDestroy() {
+    window.removeEventListener('resize', this.handleResize);
   }
 };
 </script>
-
 <style>
 .panel{
   position: relative;
--- src/components/setParam.vue
+++ src/components/setParam.vue
@@ -1,24 +1,24 @@
 <template>
     <el-form :model="form" style="max-width: 650px">
-        <el-form-item label="Param1">
-            <el-input v-model="form.param1"/>
+        <el-form-item label="B_u:">
+            <el-input v-model="form.B_u"/>
         </el-form-item>
-        <el-form-item label="Param2">
-            <el-input v-model="form.param2"/>
+        <el-form-item label="S_t:">
+            <el-input v-model="form.S_t"/>
         </el-form-item>
-        <el-form-item label="Param3">
-            <el-input v-model="form.param3"/>
+        <el-form-item label="P_w:">
+            <el-input v-model="form.P_w"/>
         </el-form-item>
-        <el-form-item label="Param4">
-            <el-input v-model="form.param4"/>
+        <el-form-item label="T_in:">
+            <el-input v-model="form.T_in"/>
         </el-form-item>
-        <el-form-item label="Method">
+        <el-form-item label="Method:">
             <el-radio-group v-model="form.method">
-                <el-radio value="method1">method1</el-radio>
-                <el-radio value="method2">method2</el-radio>
-                <el-radio value="method3">method3</el-radio>
-                <el-radio value="method4">method4</el-radio>
-                <el-radio value="method5">method5</el-radio>
+                <el-radio value="KNN">KNN</el-radio>
+                <el-radio value="nn">NN</el-radio>
+                <el-radio value="dt">Decision Tree</el-radio>
+                <el-radio value="gbdt">GBDT</el-radio>
+                <el-radio value="adaboost">AdaBoost</el-radio>
             </el-radio-group>
         </el-form-item>
         <el-form-item>
@@ -28,14 +28,16 @@
 </template>
 <script setup>
 import { ref } from 'vue'
+import axios from 'axios'
 const form = ref({
-    param1: '',
-    param2: '',
-    param3: '',
-    param4: '',
+    B_u: '',
+    S_t: '',
+    P_w: '',
+    T_in: '',
     method: ''
 })
-const onSubmit = () => {
-    console.log(form.value)
+const onSubmit = async () => {
+    const response = await axios.post('/paramstofield', form.value)
+    console.log(response.data)
 }
 </script>
